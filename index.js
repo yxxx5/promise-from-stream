07425b6b66abba03a5af6c89d161a68e61006826
yuxin <yuxin@guazi.com>
2017-03-11 17:31:37 +0800

    master

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = (stream) = {
+  return new Promise((resolve, reject) => {
+      stream.on('end', () => {
+        resolve(stream)
+      })
+      stream.on('error', (err) => {
+        reject(err)
+      })
+  })
+}
